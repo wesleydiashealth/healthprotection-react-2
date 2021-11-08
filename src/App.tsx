7d65b916203f16b19a6921f099625603e9caaf6f
Guilherme Guain <guilherme.guain@gmail.com>
2021-11-08 16:52:50 -0300

    added gtm module

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
+import TagManager from 'react-gtm-module';
 import { hotjar } from 'react-hotjar';
 
 import GlobalStyle from './styles/global';
@@ -8,6 +9,7 @@ import Routes from './routes';
 
 const App: React.FC = () => {
   useEffect(() => {
+    TagManager.initialize({ gtmId: 'GTM-NXX22MR' });
     hotjar.initialize(2690176, 6);
   }, []);
 
