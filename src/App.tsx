bb54add49314e57023ef0d760a2162323b4ecf92
Guilherme Guain <guilherme.guain@gmail.com>
2021-11-17 09:36:45 -0300

    removed gtm and hotjar (new clone)

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
-import TagManager from 'react-gtm-module';
-import { hotjar } from 'react-hotjar';
+// import TagManager from 'react-gtm-module';
+// import { hotjar } from 'react-hotjar';
 
 import GlobalStyle from './styles/global';
 
 import Routes from './routes';
 
 const App: React.FC = () => {
-  useEffect(() => {
-    TagManager.initialize({ gtmId: 'GTM-PPMT6FP' });
-    hotjar.initialize(2555854, 6);
-  }, []);
+  // useEffect(() => {
+  //   TagManager.initialize({ gtmId: 'GTM-PPMT6FP' });
+  //   hotjar.initialize(2555854, 6);
+  // }, []);
 
   return (
     <Router>
